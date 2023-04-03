c056f703237c359633ac8006edf73af95fb318c6
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-03 19:22:55 -0400

    Added Models

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(userPassword) {
+        return bcrypt.compareSync(userPassword, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
+        // FYI: format for this is 'birthday: new Date(1990, 0, 1)' as in YYYY MM DD when doing User.create
+        birthday: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3],
+            }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        freezeTableName: true,
+        modelName: 'user',
+    }
+)
+
+module.exports = User;
