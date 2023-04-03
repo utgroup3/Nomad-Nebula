783b8eadd6c56a3b4f9a7d52bf92fcd9f317c82c
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-03 17:15:35 -0400

    added some templates for back-end and other folders to follow MVC paradigms.

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+// TEMPLATE for ./controllers/api/index.js
+
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
