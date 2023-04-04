66b2501444e0b9243448a339e8db2d0df17a6622
cass <76407605+cleclair71@users.noreply.github.com>
2023-04-04 17:40:48 -0400

    edit profile routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,13 +4,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const sessionRoutes = require('./sessionRoutes');
-const profileRoutes = require('./profileRoutes'); 
+const editProfile = require('./editProfileRoutes'); 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/session', sessionRoutes); 
-router.use('/profile', profileRoutes);
+router.use('/edit-profile', editProfile);
 
 router.use((req, res) => {
   res.status(404).end();
