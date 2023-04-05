af3235927af4eab47ebc34ad1a2decbfa3e8a25a
cass <76407605+cleclair71@users.noreply.github.com>
2023-04-05 17:12:51 -0400

    sequelize error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
   Post.findAll({
