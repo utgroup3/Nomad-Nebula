53f3d2acf10b9f5606583d0325357ec8eb3c9e50
Christina <christina.e.c.barberi@gmail.com>
2023-04-05 19:38:46 -0400

    deleted my comments for less clutter

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,15 @@
-// Define an asynchronous function to handle user logout
 const logout = async () => {
-    // Send a POST request to the server to log the user out
+   
     const response = await fetch('/api/users/logout', {
-      method: 'POST', // Use the POST method to send data
-      headers: { 'Content-Type': 'application/json' }, // Set the request header to JSON format
+      method: 'POST', 
+      headers: { 'Content-Type': 'application/json' }, 
     });
   
-    // If the response status is OK (200), redirect the user to the homepage
     if (response.ok) {
       document.location.replace('/landing-page');
     } else {
-      // If there's an error, show an alert with the response status text
       alert(response.statusText);
     }
   };
   
-  // Add an event listener to the logout button to call the logout function
   document.querySelector('#logout').addEventListener('click', logout);
--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,30 +1,25 @@
-// Define an asynchronous function to handle the form submission
 const signupFormHandler = async (event) => {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
   
-    // Get the username and password from the form input fields
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    // If the username and password are not empty
+    
     if (username && password) {
-      // Send a POST request to create a new user with the given username and password
+      
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, password }), // Convert the data to JSON format
-        headers: { 'Content-Type': 'application/json' }, // Set the request header to JSON format
+        body: JSON.stringify({ username, password }), 
+        headers: { 'Content-Type': 'application/json' }, 
       });
   
-      // If the response status is OK (200)
       if (response.ok) {
-        // Redirect to the dashboard page
         document.location.replace('/profile');
       } else {
-        // Show an error message with the response status text
         alert(response.statusText);
       }
     }
   };
   
-  // Add an event listener to the form submit button to call the signupFormHandler function
+
   document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
