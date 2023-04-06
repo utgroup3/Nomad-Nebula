bf3474a64f33c5ce52335444014301faf228576a
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-06 19:10:53 -0400

    changed code around

--- public/js/zodiac.js
+++ public/js/zodiac.js
@@ -261,41 +261,11 @@ const zodiacTitleName = document.querySelector('#zodiac-title-name');
 const zodiacStarPicture = document.querySelector('#zodiac-star-picture');
 
 // Accepts input as YYYY-MM-DD
-function getZodiacSign(birthday) {
-    const month = parseInt(birthday.slice(5, 7));
-    const day = parseInt(birthday.slice(8, 10));
-    
-    if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
-      return aries;
-    } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
-      return taurus;
-    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
-      return gemini;
-    } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
-      return cancer;
-    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
-      return leo;
-    } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
-      return virgo;
-    } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
-      return libra;
-    } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
-      return scorpio;
-    } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
-      return sagittarius;
-    } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
-      return capricorn;
-    } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
-      return aquarius;
-    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
-      return pisces;
-    }
 
-}
 
 // Right now userId is hardcoded to get the first user. 
 // ** FIGURE OUT LOGIC TO REPLACE THIS CODE WITH GETTING THE CURRENT LOGGED IN USERS ID
-let userId = 1;
+let userId = 2;
 
 // Create a code that logs user's id from server-side session
 // const userId = getSessionUserId();
@@ -308,6 +278,38 @@ fetch(`/api/users/${userId}`)
     console.log(birthday);
 
     let userZodiac = getZodiacSign(birthday);
+
+    function getZodiacSign(birthday) {
+        const month = parseInt(birthday.slice(5, 7));
+        const day = parseInt(birthday.slice(8, 10));
+        
+        if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+          return aries;
+        } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+          return taurus;
+        } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+          return gemini;
+        } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+          return cancer;
+        } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+          return leo;
+        } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+          return virgo;
+        } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+          return libra;
+        } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+          return scorpio;
+        } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+          return sagittarius;
+        } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+          return capricorn;
+        } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+          return aquarius;
+        } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+          return pisces;
+        }
+    
+    }
 
     zodiacTitleName.textContent = `Your Zodiac Sign is ${userZodiac.sign}!`
 
