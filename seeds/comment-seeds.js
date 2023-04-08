0f84c97d2764bf69c4156fa8f12f9317df09bb34
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-08 00:54:49 -0400

    Fixed community to display posts and comments

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -26,6 +26,41 @@ const commentData = [
         user_id: "1",
         post_id: "5",
     },
+    {
+        comment: "DAMN THIS COMMENT",
+        user_id: "3",
+        post_id: "5",
+    },
+    {
+        comment: "IM A COMMENT",
+        user_id: "1",
+        post_id: "4",
+    },
+    {
+        comment: "ANYONE LIKE COMMENTS?",
+        user_id: "5",
+        post_id: "4",
+    },
+    {
+        comment: "I am THE comment",
+        user_id: "1",
+        post_id: "1",
+    },
+    {
+        comment: "issa me, comment",
+        user_id: "3",
+        post_id: "4",
+    },
+    {
+        comment: "comments, anyone?",
+        user_id: "5",
+        post_id: "3",
+    },
+    {
+        comment: "Im in love with comments",
+        user_id: "4",
+        post_id: "2",
+    },
 ]
 
 const seedComment = () => Comment.bulkCreate(commentData);
