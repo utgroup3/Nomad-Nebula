709fb24fedfbae6101c9d1294374f16a3eb8c6a4
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-07 23:00:33 -0400

    should have the updated views and css and adding a post should work because it works on mine ;-)

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -5,9 +5,6 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="title"]').value;
   const body = document.querySelector('textarea[name="content"]').value;
 
-  console.log(title);
-  console.log(body);
-
   try {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
@@ -36,42 +33,3 @@ async function newFormHandler(event) {
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
-// async function newFormHandler(event) {
-//   console.log('Form submitted');
-//   event.preventDefault();
-
-//   const title = document.querySelector('input[name="title"]').value;
-//   const body = document.querySelector('textarea[name="text"]').value;
-//   const imageInput = document.querySelector('input[name="image"]');
-
-//   // Create a FormData object to handle the image upload
-//   const formData = new FormData();
-//   formData.append('title', title);
-//   formData.append('content', body);
-
-//   if (imageInput.files[0]) {
-//     formData.append('image', imageInput.files[0]);
-//   }
-
-//   try {
-//     const response = await fetch(`/api/posts`, {
-//       method: 'POST',
-//       body: formData, // Use formData object as the body
-//     });
-
-//     if (response.ok) {
-//       console.log('Post created successfully!');
-
-//       // Redirect to dashboard
-//       document.location.replace('/profile');
-//     } else {
-//       console.log('Failed to create post');
-//       alert(response.statusText);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     alert('Failed to create post');
-//   }
-// }
-
-// document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
