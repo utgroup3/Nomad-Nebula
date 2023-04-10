5bde056e87d71038f1da2de88f3a80a38987d365
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-10 12:48:42 -0400

    added filelist with upload buttons

--- public/js/editProfile.js
+++ public/js/editProfile.js
@@ -22,3 +22,17 @@ if (form) {
       });
   };
 }
+
+// This portion of code is to incorporate a filelist beside image upload
+
+const fileInput = document.querySelector('input[type="file"]');
+const fileUploadLabel = document.querySelector('.file-upload-label');
+
+fileInput.addEventListener('change', (event) => {
+  // Get the selected file name
+  console.log(fileInput.files)
+  const fileName = fileInput.files[0].name;
+
+  // Set the file upload label to the selected file name
+  fileUploadLabel.textContent = fileName;
+});
