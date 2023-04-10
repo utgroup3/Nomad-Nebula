fc83729dcea986df22eeda31a519c7fda05d1447
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-10 15:34:35 -0400

    postInfo update

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,44 +1,99 @@
-// Handle form submission
-const editPostForm = document.querySelector('.edit-post-form');
-editPostForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const title = editPostForm.querySelector('[name="post-title"]').value;
-  const post_content = editPostForm.querySelector('[name="post-body"]').value;
-  const postId = editPostForm.dataset.postId;
+// Handle click event on post links
+document.querySelectorAll('.post-link').forEach((postLink) => {
+  postLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    const buttonsContainer = event.currentTarget.querySelector('.buttons');
+    buttonsContainer.classList.toggle('is-hidden');
+  });
+});
 
-  // Make API request to update post
-  try {
-    const response = await fetch(`/api/posts/${postId}`, {
-      method: 'PUT',
-      body: JSON.stringify({ title, post_content }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    if (response.ok) {
-      window.location.href = '/dashboard';
-    } else {
-      throw new Error('Failed to update post');
+// Handle save changes button click
+document.querySelectorAll('.save-changes-btn').forEach((saveBtn) => {
+  saveBtn.addEventListener('click', async (event) => {
+    const postId = event.currentTarget.closest('.post-link').dataset.postId;
+    const title = event.currentTarget.closest('.card').querySelector('.card-header-title').innerText;
+    const post_content = event.currentTarget.closest('.card').querySelector('.content').innerText;
+
+    // Make API request to update post
+    try {
+      const response = await fetch(`/api/posts/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, post_content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        window.location.href = '/profile';
+      } else {
+        throw new Error('Failed to update post');
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
 
 // Handle delete post button click
-const deletePostButton = document.querySelector('.delete-post-btn');
-deletePostButton.addEventListener('click', async () => {
-  const postId = editPostForm.dataset.postId;
+document.querySelectorAll('.delete-btn').forEach((deleteBtn) => {
+  deleteBtn.addEventListener('click', async (event) => {
+    const postId = event.currentTarget.closest('.post-link').dataset.postId;
 
-  // Make API request to delete post
-  try {
-    const response = await fetch(`/api/posts/${postId}`, {
-      method: 'DELETE'
-    });
-    if (response.ok) {
-      window.location.href = '/dashboard';
-    } else {
-      throw new Error('Failed to delete post');
+    // Make API request to delete post
+    try {
+      const response = await fetch(`/api/posts/${postId}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        window.location.href = '/profile';
+      } else {
+        throw new Error('Failed to delete post');
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
+
+// // Handle form submission
+// const editPostForm = document.querySelector('.edit-post-form');
+// editPostForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+//   const title = editPostForm.querySelector('[name="post-title"]').value;
+//   const post_content = editPostForm.querySelector('[name="post-body"]').value;
+//   const postId = editPostForm.dataset.postId;
+
+//   // Make API request to update post
+//   try {
+//     const response = await fetch(`/api/posts/${postId}`, {
+//       method: 'PUT',
+//       body: JSON.stringify({ title, post_content }),
+//       headers: { 'Content-Type': 'application/json' }
+//     });
+//     if (response.ok) {
+//       window.location.href = '/dashboard';
+//     } else {
+//       throw new Error('Failed to update post');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// // Handle delete post button click
+// const deletePostButton = document.querySelector('.delete-post-btn');
+// deletePostButton.addEventListener('click', async () => {
+//   const postId = editPostForm.dataset.postId;
+
+//   // Make API request to delete post
+//   try {
+//     const response = await fetch(`/api/posts/${postId}`, {
+//       method: 'DELETE'
+//     });
+//     if (response.ok) {
+//       window.location.href = '/dashboard';
+//     } else {
+//       throw new Error('Failed to delete post');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
