e8cff80bb7dd2e184e31e64f063ba242d1e79ea2
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-10 01:35:10 -0400

    userlikes and allpost have same functionalities now with an update comment feature. changed the userlike layout to match the allpost.

--- public/js/updateCommentUserLikes.js
+++ public/js/updateCommentUserLikes.js
@@ -1,59 +1,57 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.addEventListener('click', async (event) => {
-    if (event.target.matches('.update-comment')) {
-      event.preventDefault();
+document.addEventListener('click', async (event) => {
+  if (event.target.matches('.update-comment')) {
+    event.preventDefault();
 
-      const commentId = event.target.dataset.commentId;
-      const commentContainer = event.target.closest('.comment-container');
-      const commentTextElement = commentContainer.querySelector('p');
-      const currentCommentText = commentTextElement.textContent.trim();
+    const commentId = event.target.dataset.commentId;
+    const commentContainer = event.target.closest('.comment-container');
+    const commentTextElement = commentContainer.querySelector('p');
+    const currentCommentText = commentTextElement.textContent.trim();
 
-      const modal = document.getElementById('update-comment-modal');
-      const textarea = document.getElementById('update-comment-textarea');
-      textarea.value = currentCommentText;
-      modal.classList.add('is-active');
+    const modal = document.getElementById('update-comment-modal');
+    const textarea = document.getElementById('update-comment-textarea');
+    textarea.value = currentCommentText;
+    modal.classList.add('is-active');
 
-      document.getElementById('submit-update-comment').onclick = async () => {
-        const updatedCommentText = textarea.value.trim();
-        if (updatedCommentText === '') {
-          return;
-        }
-        modal.classList.remove('is-active');
-        updateComment(updatedCommentText, commentId, commentTextElement);
-      };
-
-      document.getElementById('cancel-update-comment').onclick = () => {
-        modal.classList.remove('is-active');
-      };
-
-      document.querySelector('.modal-background').onclick = () => {
-        modal.classList.remove('is-active');
-      };
-
-      document.querySelector('.delete').onclick = () => {
-        modal.classList.remove('is-active');
-      };
-    }
-  });
-
-  async function updateComment(updatedCommentText, commentId, commentTextElement) {
-    try {
-      const response = await fetch(`/api/comments/${commentId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ comment: updatedCommentText }),
-      });
-
-      if (response.ok) {
-        const updatedComment = await response.json();
-        commentTextElement.textContent = updatedComment.comment;
-      } else {
-        alert('Error updating comment.');
+    document.getElementById('submit-update-comment').onclick = async () => {
+      const updatedCommentText = textarea.value.trim();
+      if (updatedCommentText === '') {
+        return;
       }
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-    }
+      modal.classList.remove('is-active');
+      updateComment(updatedCommentText, commentId, commentTextElement);
+    };
+
+    document.getElementById('cancel-update-comment').onclick = () => {
+      modal.classList.remove('is-active');
+    };
+
+    document.querySelector('.modal-background').onclick = () => {
+      modal.classList.remove('is-active');
+    };
+
+    document.querySelector('.delete').onclick = () => {
+      modal.classList.remove('is-active');
+    };
   }
 });
+
+async function updateComment(updatedCommentText, commentId, commentTextElement) {
+  try {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ comment: updatedCommentText }),
+    });
+
+    if (response.ok) {
+      const updatedComment = await response.json();
+      commentTextElement.textContent = updatedComment.comment;
+    } else {
+      alert('Error updating comment.');
+    }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
