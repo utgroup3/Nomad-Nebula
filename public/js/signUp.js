fcc3ab9af0d1f06749537da2db21d05ef36cf618
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-11 21:46:27 -0400

    heroku commit

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -15,7 +15,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/login');
+        document.location.replace('/profile');
       } else {
         alert(response.statusText);
       }
