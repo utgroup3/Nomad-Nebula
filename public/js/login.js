9038e5be845adfa64985b1f1605e34b847afdc03
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-11 23:21:09 -0400

    just changes the format of js

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,12 @@
 const loginFormHandler = async (event) => {
-  
+
   event.preventDefault();
 
   const username = document.querySelector('#username').value.trim();
   const password = document.querySelector('#password').value.trim();
 
   if (username && password) {
-    
+
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
