de3464e9058b31d73b19cbb6db51efec595aba8e
Calvin Trieu <calvin.trieu@hotmail.com>
2023-04-11 20:57:08 -0400

    Deploying to Heroku

--- postinstall.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-console.log('Running schema.sql');
-execSync('psql -f db/schema.sql');
-
-console.log('Running seeds');
-execSync('node seeds/index.js');
